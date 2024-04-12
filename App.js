0030ac7a69063e7e84392318d69c99a6b0bb9134
Maggie Collette <collette.m@northeastern.edu>
2024-04-12 12:38:32 -0400

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 import session from "express-session";
 import "dotenv/config";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+mongoose.connect(process.env.DB_CONNECTION_STRING);
 const app = express()
 app.use(
   cors({
